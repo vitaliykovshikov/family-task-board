608f72c55ed13dde057aa20954e82d6abb2b3f4c
Vitalii <v_kovshykov@wargaming.net>
2026-05-05 20:48:09 +0300

    Configure Supabase project

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://sqqessqrhguxfpxjbwfq.supabase.co",
+  supabaseAnonKey: "sb_publishable_kCaIaE0kGWqMRh9CHCS4KQ_zdLuEDcn",
 };
